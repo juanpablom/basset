f0001db7ee91fe50e59af403180c655aba99da6e
Estanislau Trepat <estanis@etrepat.com>
2014-02-20 22:05:20 +0100

    Bump version.

--- src/Basset/BassetServiceProvider.php
+++ src/Basset/BassetServiceProvider.php
@@ -18,7 +18,7 @@ class BassetServiceProvider extends ServiceProvider {
      *
      * @var string
      */
-    const VERSION = '4.0.1';
+    const VERSION = '4.0.2';
 
     /**
      * Indicates if loading of the provider is deferred.
