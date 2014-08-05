98adc99e1a896c70b5ff3c5e4abe0e82e6a1d9b0
pthurmond <p_thurmond@yahoo.com>
2014-08-05 13:52:07 -0500

    Fixed problem with unit tests. Adjusted so that they account for the code changes.

--- tests/Basset/Builder/BuilderTest.php
+++ tests/Basset/Builder/BuilderTest.php
@@ -3,42 +3,36 @@
 use Mockery as m;
 
 class BuilderTest extends PHPUnit_Framework_TestCase {
-    var $buildPattern = '[collection-name]-[date:m-d-Y]-release1';
 
     /**
-     * Tear down operations
+     * Tear down
      */
     public function tearDown()
     {
-        #m::close();
+        m::close();
     }
 
     /**
-     * Setup operations
+     * Setup
      */
     public function setUp()
     {
         $this->files = m::mock('Illuminate\Filesystem\Filesystem');
         $this->files->shouldReceive('exists')->once()->with('foo')->andReturn(true);
-        $this->files->shouldReceive('put')->andReturn(1);
-
         $this->manifest = m::mock('Basset\Manifest\Manifest');
-        $this->manifest->shouldReceive('make')->andReturn(new \Basset\Manifest\Entry());
-
-        $this->directory = m::mock('Basset\Directory');
         $this->collection = m::mock('Basset\Collection');
-        $this->builder = new Basset\Builder\Builder($this->files, $this->manifest, 'foo', $this->buildPattern);
+        $this->builder = new Basset\Builder\Builder($this->files, $this->manifest, 'foo');
     }
 
     /**
-     *
+     * testBuilderChecksForBuildPathAndMakesDirectoryIfItDoesNotExist()
      */
     public function testBuilderChecksForBuildPathAndMakesDirectoryIfItDoesNotExist()
     {
         $this->files->shouldReceive('exists')->once()->with('foo')->andReturn(false);
         $this->files->shouldReceive('makeDirectory')->once()->with('foo')->andReturn(true);
 
-        $builder = new Basset\Builder\Builder($this->files, $this->manifest, 'foo', $this->buildPattern);
+        $builder = new Basset\Builder\Builder($this->files, $this->manifest, 'foo');
     }
 
     /**
@@ -61,91 +55,88 @@ class BuilderTest extends PHPUnit_Framework_TestCase {
      */
     public function testBuildingExistingProductionCollectionThrowsBuildNotRequiredException()
     {
-        $this->collection->shouldReceive('getAssetsWithoutRaw')->once()->with('stylesheets')->andReturn(new Illuminate\Support\Collection(array(
-            $asset = m::mock('Basset\Asset')
-        )));
+        $asset = m::mock('Basset\Asset');
+        $assetCollection = new Illuminate\Support\Collection( array($asset) );
+        $this->collection->shouldReceive('getAssetsWithoutRaw')->once()->with('stylesheets')->andReturn($assetCollection);
         $asset->shouldReceive('build')->once()->andReturn('body { }');
 
-        $this->collection->shouldReceive('getIdentifier')->once()->andReturn('foo');
+        $this->collection->shouldReceive('getIdentifier')->twice()->andReturn('foo');
         $this->collection->shouldReceive('getExtension')->once()->with('stylesheets')->andReturn('css');
 
         $entry = m::mock('Basset\Manifest\Entry');
         $this->manifest->shouldReceive('make')->once()->with('foo')->andReturn($entry);
-        $entry->shouldReceive('getProductionFingerprint')->with('stylesheets')->andReturn($fingerprint = 'foo-'.md5('body { }').'.css');
 
-        $this->files->shouldReceive('exists')->once()->with('foo/'.$fingerprint)->andReturn(true);
+        $fingerprint = 'foo-' . md5('body { }') . '.css';
+        $entry->shouldReceive('getProductionFingerprint')->with('stylesheets')->andReturn($fingerprint);
+
+        $this->files->shouldReceive('exists')->once()->with('foo/' . $fingerprint)->andReturn(true);
 
         $this->builder->buildAsProduction($this->collection, 'stylesheets');
     }
 
-    /**
-     * Test building a production collection and writes tot he file system and set a production fingerprint
-     */
     public function testBuildingProductionCollectionWritesToFilesystemAndSetsProductionFingerprint()
     {
-        $newCollection = new Illuminate\Support\Collection( array( $asset = m::mock('Basset\Asset') ) );
-        $this->collection->shouldReceive('getAssetsWithoutRaw')->once()->with('stylesheets')->andReturn($newCollection);
+        $asset = m::mock('Basset\Asset');
+        $assetCollection = new Illuminate\Support\Collection( array($asset) );
+        $this->collection->shouldReceive('getAssetsWithoutRaw')->once()->with('stylesheets')->andReturn($assetCollection);
         $asset->shouldReceive('build')->once()->andReturn('body { }');
 
-        $this->collection->shouldReceive('getIdentifier')->once()->andReturn('foo');
+        $this->collection->shouldReceive('getIdentifier')->twice()->andReturn('foo');
         $this->collection->shouldReceive('getExtension')->once()->with('stylesheets')->andReturn('css');
 
-        $fingerprint = 'foo-'.md5('body { }').'.css';
+        $fingerprint = 'foo-' . md5('body { }') . '.css';
 
-        $this->manifest->shouldReceive('make')->once()->with('foo')->andReturn($entry = m::mock('Basset\Manifest\Entry'));
+        $entry = m::mock('Basset\Manifest\Entry');
+        $this->manifest->shouldReceive('make')->once()->with('foo')->andReturn($entry);
         $entry->shouldReceive('getProductionFingerprint')->with('stylesheets')->andReturn(null);
         $entry->shouldReceive('setProductionFingerprint')->with('stylesheets', $fingerprint);
 
-        $this->files->shouldReceive('put')->once()->with('foo/'.$fingerprint, 'body { }');
+        $this->files->shouldReceive('put')->once()->with('foo/'. $fingerprint, 'body { }');
 
         $this->builder->buildAsProduction($this->collection, 'stylesheets');
     }
 
-    /**
-     * Test building a production collection with the force parameter
-     */
     public function testBuildingProductionCollectionWithForce()
     {
-        $this->collection->shouldReceive('getAssetsWithoutRaw')->once()->with('stylesheets')->andReturn(new Illuminate\Support\Collection(array(
-            $asset = m::mock('Basset\Asset')
-        )));
+        $asset = m::mock('Basset\Asset');
+        $assetCollection = new Illuminate\Support\Collection( array($asset) );
+        $this->collection->shouldReceive('getAssetsWithoutRaw')->once()->with('stylesheets')->andReturn($assetCollection);
         $asset->shouldReceive('build')->once()->andReturn('body { }');
 
-        $this->collection->shouldReceive('getIdentifier')->once()->andReturn('foo');
+        $this->collection->shouldReceive('getIdentifier')->twice()->andReturn('foo');
         $this->collection->shouldReceive('getExtension')->once()->with('stylesheets')->andReturn('css');
 
-        $fingerprint = 'foo-'.md5('body { }').'.css';
+        $fingerprint = 'foo-' . md5('body { }') . '.css';
 
-        $this->manifest->shouldReceive('make')->once()->with('foo')->andReturn($entry = m::mock('Basset\Manifest\Entry'));
+        $entry = m::mock('Basset\Manifest\Entry');
+        $this->manifest->shouldReceive('make')->once()->with('foo')->andReturn($entry);
         $entry->shouldReceive('getProductionFingerprint')->with('stylesheets')->andReturn($fingerprint);
         $entry->shouldReceive('setProductionFingerprint')->with('stylesheets', $fingerprint);
 
-        $this->files->shouldReceive('put')->once()->with('foo/'.$fingerprint, 'body { }');
+        $this->files->shouldReceive('put')->once()->with('foo/' . $fingerprint, 'body { }');
 
         $this->builder->setForce(true);
         $this->builder->buildAsProduction($this->collection, 'stylesheets');
     }
 
-    /**
-     * Test building production collection using gzip
-     */
     public function testBuildingProductionCollectionWithGzip()
     {
-        $this->collection->shouldReceive('getAssetsWithoutRaw')->once()->with('stylesheets')->andReturn(new Illuminate\Support\Collection(array(
-            $asset = m::mock('Basset\Asset')
-        )));
+        $asset = m::mock('Basset\Asset');
+        $assetCollection = new Illuminate\Support\Collection( array($asset) );
+        $this->collection->shouldReceive('getAssetsWithoutRaw')->once()->with('stylesheets')->andReturn($assetCollection);
         $asset->shouldReceive('build')->once()->andReturn('body { }');
 
-        $this->collection->shouldReceive('getIdentifier')->once()->andReturn('foo');
+        $this->collection->shouldReceive('getIdentifier')->twice()->andReturn('foo');
         $this->collection->shouldReceive('getExtension')->once()->with('stylesheets')->andReturn('css');
 
-        $fingerprint = 'foo-'.md5('body { }').'.css';
+        $fingerprint = 'foo-' . md5('body { }') . '.css';
 
-        $this->manifest->shouldReceive('make')->once()->with('foo')->andReturn($entry = m::mock('Basset\Manifest\Entry'));
+        $entry = m::mock('Basset\Manifest\Entry');
+        $this->manifest->shouldReceive('make')->once()->with('foo')->andReturn($entry);
         $entry->shouldReceive('getProductionFingerprint')->with('stylesheets')->andReturn(null);
         $entry->shouldReceive('setProductionFingerprint')->with('stylesheets', $fingerprint);
 
-        $this->files->shouldReceive('put')->once()->with('foo/'.$fingerprint, gzencode('body { }', 9));
+        $this->files->shouldReceive('put')->once()->with('foo/' . $fingerprint, gzencode('body { }', 9));
 
         $this->builder->setGzip(true);
         $this->builder->buildAsProduction($this->collection, 'stylesheets');
@@ -159,7 +150,8 @@ class BuilderTest extends PHPUnit_Framework_TestCase {
         $this->collection->shouldReceive('getAssetsWithoutRaw')->once()->with('stylesheets')->andReturn(new Illuminate\Support\Collection);
         $this->collection->shouldReceive('getIdentifier')->once()->andReturn('foo');
 
-        $this->manifest->shouldReceive('make')->once()->with('foo')->andReturn($entry = m::mock('Basset\Manifest\Entry'));
+        $entry = m::mock('Basset\Manifest\Entry');
+        $this->manifest->shouldReceive('make')->once()->with('foo')->andReturn($entry);
         $entry->shouldReceive('hasDevelopmentAssets')->once()->with('stylesheets')->andReturn(false);
         $entry->shouldReceive('resetDevelopmentAssets')->once()->with('stylesheets');
 
@@ -182,7 +174,8 @@ class BuilderTest extends PHPUnit_Framework_TestCase {
         $assets[1]->shouldReceive('getRelativePath')->once()->andReturn('bar/qux.css');
         $assets[1]->shouldReceive('getBuildPath')->once()->andReturn('bar/qux-321.css');
 
-        $this->manifest->shouldReceive('make')->once()->with('foo')->andReturn($entry = m::mock('Basset\Manifest\Entry'));
+        $entry = m::mock('Basset\Manifest\Entry');
+        $this->manifest->shouldReceive('make')->once()->with('foo')->andReturn($entry);
         $entry->shouldReceive('hasDevelopmentAsset')->once()->with($assets[0])->andReturn(true);
         $entry->shouldReceive('getDevelopmentAsset')->once()->with($assets[0])->andReturn('bar/baz-123.css');
         $entry->shouldReceive('hasDevelopmentAsset')->once()->with($assets[1])->andReturn(true);
@@ -198,7 +191,7 @@ class BuilderTest extends PHPUnit_Framework_TestCase {
     }
 
     /**
-     * Test building a development collection with no current manifest entry.
+     * testBuildingDevelopmentCollectionWithNoCurrentManifestEntry()
      */
     public function testBuildingDevelopmentCollectionWithNoCurrentManifestEntry()
     {
@@ -206,29 +199,20 @@ class BuilderTest extends PHPUnit_Framework_TestCase {
             $assets[] = m::mock('Basset\Asset'),
             $assets[] = m::mock('Basset\Asset')
         )));
-
-        $group = 'stylesheets';
-        $mockSupportCollection = m::mock('\Illuminate\Support\Collection');
-        $mockSupportCollection->shouldReceive('getGroup')->andReturn($group);
-
         $this->collection->shouldReceive('getIdentifier')->once()->andReturn('foo');
-        $this->collection->shouldReceive('getAssetsWithoutRaw')->with($group)->andReturn($mockSupportCollection);
 
         $entry = m::mock('Basset\Manifest\Entry');
-        $entry->shouldReceive('addDevelopmentAsset')->andReturn(true);
+        $this->manifest->shouldReceive('make')->once()->with('foo')->andReturn($entry);
         $entry->shouldReceive('hasDevelopmentAssets')->once()->with('stylesheets')->andReturn(false);
         $entry->shouldReceive('resetDevelopmentAssets')->once()->with('stylesheets');
 
         $assets[0]->shouldReceive('getBuildPath')->once()->andReturn('bar/baz-123.css');
         $assets[0]->shouldReceive('build')->once()->andReturn('body { }');
-
         $assets[1]->shouldReceive('getBuildPath')->once()->andReturn('bar/qux-321.css');
         $assets[1]->shouldReceive('build')->once()->andReturn('html { }');
 
         $entry->shouldReceive('addDevelopmentAsset')->once()->with($assets[0]);
         $entry->shouldReceive('addDevelopmentAsset')->once()->with($assets[1]);
-
-        $this->manifest->shouldReceive('make')->once()->with('foo')->andReturn($entry);
 
         $this->files->shouldReceive('exists')->once()->with('foo/foo/bar')->andReturn(false);
         $this->files->shouldReceive('exists')->once()->with('foo/foo/bar')->andReturn(true);
@@ -237,11 +221,11 @@ class BuilderTest extends PHPUnit_Framework_TestCase {
         $this->files->shouldReceive('put')->once()->with('foo/foo/bar/baz-123.css', 'body { }');
         $this->files->shouldReceive('put')->once()->with('foo/foo/bar/qux-321.css', 'html { }');
 
-        $this->builder->buildAsDevelopment($this->collection, $group);
+        $this->builder->buildAsDevelopment($this->collection, 'stylesheets');
     }
 
     /**
-     * Test building development collection with no changes while using force parameter.
+     * testBuildingDevelopmentCollectionWithNoChangesButWithForcing()
      */
     public function testBuildingDevelopmentCollectionWithNoChangesButWithForcing()
     {
@@ -249,6 +233,7 @@ class BuilderTest extends PHPUnit_Framework_TestCase {
             $assets[] = m::mock('Basset\Asset'),
             $assets[] = m::mock('Basset\Asset')
         )));
+
         $this->collection->shouldReceive('getIdentifier')->once()->andReturn('foo');
 
         $assets[0]->shouldReceive('getRelativePath')->once()->andReturn('bar/baz.css');
@@ -258,7 +243,8 @@ class BuilderTest extends PHPUnit_Framework_TestCase {
         $assets[1]->shouldReceive('getBuildPath')->once()->andReturn('bar/qux-321.css');
         $assets[1]->shouldReceive('build')->once()->andReturn('html { }');
 
-        $this->manifest->shouldReceive('make')->once()->with('foo')->andReturn($entry = m::mock('Basset\Manifest\Entry'));
+        $entry = m::mock('Basset\Manifest\Entry');
+        $this->manifest->shouldReceive('make')->once()->with('foo')->andReturn($entry);
         $entry->shouldReceive('hasDevelopmentAssets')->once()->with('stylesheets')->andReturn(true);
         $entry->shouldReceive('resetDevelopmentAssets')->once()->with('stylesheets');
         $entry->shouldReceive('getDevelopmentAssets')->once()->with('stylesheets')->andReturn(array(
@@ -281,20 +267,21 @@ class BuilderTest extends PHPUnit_Framework_TestCase {
     }
 
     /**
-     * Test building development collection with gzip.
+     * testBuildingDevelopmentCollectionWithGzip()
      */
     public function testBuildingDevelopmentCollectionWithGzip()
     {
-        $this->collection->shouldReceive('getAssetsWithoutRaw')->once()->with('stylesheets')->andReturn(new Illuminate\Support\Collection(array(
-            $asset = m::mock('Basset\Asset')
-        )));
+        $asset = m::mock('Basset\Asset');
+        $assetCollection = new Illuminate\Support\Collection( array($asset) );
+        $this->collection->shouldReceive('getAssetsWithoutRaw')->once()->with('stylesheets')->andReturn($assetCollection);
         $this->collection->shouldReceive('getIdentifier')->once()->andReturn('foo');
 
         $asset->shouldReceive('getRelativePath')->once()->andReturn('bar/baz.css');
         $asset->shouldReceive('getBuildPath')->once()->andReturn('bar/baz-123.css');
         $asset->shouldReceive('build')->once()->andReturn('body { }');
 
-        $this->manifest->shouldReceive('make')->once()->with('foo')->andReturn($entry = m::mock('Basset\Manifest\Entry'));
+        $entry = m::mock('Basset\Manifest\Entry');
+        $this->manifest->shouldReceive('make')->once()->with('foo')->andReturn($entry);
         $entry->shouldReceive('hasDevelopmentAssets')->once()->with('stylesheets')->andReturn(true);
         $entry->shouldReceive('getDevelopmentAssets')->once()->with('stylesheets')->andReturn(array('bar/baz.css' => 'bar/baz-123.css'));
 
@@ -302,7 +289,6 @@ class BuilderTest extends PHPUnit_Framework_TestCase {
         $entry->shouldReceive('addDevelopmentAsset')->once()->with($asset);
 
         $this->files->shouldReceive('exists')->once()->with('foo/foo/bar')->andReturn(true);
-
         $this->files->shouldReceive('put')->once()->with('foo/foo/bar/baz-123.css', gzencode('body { }', 9));
 
         $this->builder->setGzip(true);
